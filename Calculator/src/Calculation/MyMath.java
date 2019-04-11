8a2f60c95b2bed1a8bf8ea951579d34261aa1b1d
localadmin <localadmin@DESKTOP-TJSA2EI>
2019-04-11 12:15:51 +0200

    Added functions to MyMath

--- Calculator/src/Calculation/MyMath.java
+++ Calculator/src/Calculation/MyMath.java
@@ -15,4 +15,36 @@ public class MyMath {
     return result;
     }
     
+    public static double sub(double firstnum, double secondnum){
+    double result = firstnum-secondnum;
+    return result;
+    }
+    
+    public static double mul(double firstnum, double secondnum){
+    double result = firstnum*secondnum;
+    return result;
+    }
+    
+    public static double div(double firstnum, double secondnum){
+    double result = firstnum/secondnum;
+    return result;
+    }
+    
+    public static double fac(double num){
+    double i = num; //counter pre faktorial
+    double result;
+    if(num == 0)
+        result = 1;
+    else if(num == 1)
+        result = num;
+    else{
+    while(i>1){
+    i=i-1;
+    num = num*i;
+    }
+    result = num;
+    }
+    return result;
+    }
+    
 }
