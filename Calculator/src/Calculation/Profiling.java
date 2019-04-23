32201efb369d84fe0ed7b300e3ccca0f0d0da373
localadmin <localadmin@DESKTOP-TJSA2EI>
2019-04-23 12:26:39 +0200

    Rozsirenie funkcionality profilingu

--- Calculator/src/Calculation/Profiling.java
+++ Calculator/src/Calculation/Profiling.java
@@ -5,7 +5,6 @@
  */
 package Calculation;
 import java.util.*;
-import java.io.*;
 
 /**
  *
@@ -15,49 +14,36 @@ public class Profiling {
     public static void main(String[] args){
         if(args.length == 0)
         {
-            System.err.format("Zadajte subor alebo hodnoty\n");
+            System.err.format("Zadajte hodnoty\n");
             System.exit(0);
         }
-        else{
-            //      System.out.println(args[0]);
-        java.io.File file = new java.io.File(args[0]);       
-        List<Integer> list = new ArrayList<Integer>();
-       // int[] pole = new int[100];
-        int i =0; int sum = 0; int mean; int variance = 0; double deviation =0;
-        try {
-            Scanner input = new Scanner(file);
-            while (input.hasNext()){
-                i++;
-               // int[] copy = Arrays.copyOf(pole, pole.length+1);
-                Integer num = input.nextInt();
-                sum = sum + num;
-                list.add(num);
-                //copy[i] = num;    
+        else{      
+        List<Double> list = new ArrayList<Double>();
+        int i =0; double sum = 0; double mean; double variance = 0; double deviation =0;
+            Scanner input = new Scanner(System.in);
+            int pocet = args.length;
+            for ( int p = 0; p<pocet; p++)
+            {
+              i++;
+              Double num = Double.parseDouble(args[p]);
+              sum = sum + num;
+              list.add(num);
             }
             mean = sum/i;
-            System.out.println("sucet je: "+ sum);
-            System.out.println("priemer je: "+ mean);
-            
             for (int n = 0; n < list.size(); n++) {
                 if (list.get(n) < mean){
-                    int clen = mean - list.get(n);
+                    double clen = mean - list.get(n);
                     variance = variance + (clen*clen);
                 }
                 else{
-                    int clen = list.get(n) - mean;
+                    double clen = list.get(n) - mean;
                     variance = variance + (clen*clen);
                 }
-    //            System.out.print((list.get(n)).toString()+" ");
             }
                 variance = variance/i;
-                System.out.println("variancia je: "+ variance);
-                deviation = Math.sqrt(variance);
-                System.out.println("deviacia je: "+ deviation);
+                deviation = MyMath.sqrt(variance);
+                System.out.println(deviation);
         }    
-        catch (FileNotFoundException e){
-                System.err.format("File not found\n");
-                }
         }
     }
-}
 
